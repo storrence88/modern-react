eae7fa0da8b466fc5d64fdcbc1c5accd93795176
Steven Torrence <storrence88@gmail.com>
2020-04-23 20:46:06 -0500

    Initial commit

---
+++ 01-a-taste-of-react/index.js
@@ -0,0 +1,13 @@
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Hello />, document.querySelector('#root'));
