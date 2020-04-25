c40b6a776c63a0fec49dd08e659aed95449025d5
Steven Torrence <storrence88@gmail.com>
2020-04-24 21:55:02 -0500

    Standard React App Layout

---
+++ 02-introducing-jsx/02-app-structure/NumPicker.js
@@ -0,0 +1,26 @@
+function getNum() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+class NumPicker extends React.Component {
+  render() {
+    const num = getNum();
+    let msg;
+    if (num === 7) {
+      msg = (
+        <div>
+          <h2>Congratulations, you win!</h2>
+          <img src='https://media.giphy.com/media/5oGIdt1xapQ76/giphy.gif' />
+        </div>
+      );
+    } else {
+      msg = <p>Sorry! Please try again...</p>;
+    }
+    return (
+      <div>
+        <h1>Your number is...{num}</h1>
+        {msg}
+      </div>
+    );
+  }
+}
