d975ccbe4e71822758a267d28686aafbce70db41
Steven Torrence <storrence88@gmail.com>
2020-04-28 22:32:17 -0500

    Complete the logic for adding the Pokegame component

--- 05-pokedex-project/pokegame-demo/src/App.js
+++ 05-pokedex-project/pokegame-demo/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className='App'>
         <h1 className='App-title'>Pokedex</h1>
-        <Pokedex />
+        <Pokegame />
       </div>
     );
   }
--- 05-pokedex-project/pokegame-demo/src/Pokedex.js
+++ 05-pokedex-project/pokegame-demo/src/Pokedex.js
@@ -3,25 +3,18 @@ import Pokecard from './Pokecard';
 import './Pokedex.css';
 
 class Pokedex extends Component {
-  static defaultProps = {
-    pokemon: [
-      { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
-      { id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
-      { id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
-      { id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
-      { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
-      { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
-      { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
-      { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 }
-    ]
-  };
   render() {
-    const { pokemon } = this.props;
+    const { pokemon, exp, isWinner } = this.props;
     return (
       <div className='Pokedex'>
-        {pokemon.map((poke) => (
-          <Pokecard id={poke.id} name={poke.name} type={poke.type} exp={poke.base_experience} />
-        ))}
+        <h1>Pokedex!</h1>
+        <p>Total EXP: {exp}</p>
+        <p>{isWinner ? 'Winner' : 'Loser'}</p>
+        <div className='Pokedex-cards'>
+          {pokemon.map((poke) => (
+            <Pokecard id={poke.id} name={poke.name} type={poke.type} exp={poke.base_experience} />
+          ))}
+        </div>
       </div>
     );
   }
