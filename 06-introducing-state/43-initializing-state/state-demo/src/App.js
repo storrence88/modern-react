f1078f9fe2e570b2cbfae333c9d7ecc4b0cc24b9
Steven Torrence <storrence88@gmail.com>
2020-04-30 21:43:47 -0500

    Setting state correctly

--- 06-introducing-state/43-initializing-state/state-demo/src/App.js
+++ 06-introducing-state/43-initializing-state/state-demo/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Game from './Game';
+import Rando from './Rando';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <Game />
+      <Rando maxNum={7} />
     </div>
   );
 }
