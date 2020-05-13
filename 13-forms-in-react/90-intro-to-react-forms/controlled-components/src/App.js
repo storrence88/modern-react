229982fac5e0604e3b9ef83119fb58010016e32e
Steven Torrence <storrence88@gmail.com>
2020-05-12 22:35:26 -0500

    Multiple inputs

--- 13-forms-in-react/90-intro-to-react-forms/controlled-components/src/App.js
+++ 13-forms-in-react/90-intro-to-react-forms/controlled-components/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MultipleForm from './MultipleForm';
 import Form from './Form';
 import './App.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Form />
+        <MultipleForm />
       </div>
     );
   }
