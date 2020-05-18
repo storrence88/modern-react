f24cb3b1788ff605d7eda1271209d8ebedffcd01
Steven Torrence <storrence88@gmail.com>
2020-05-17 21:10:02 -0500

    Create Todo app and add button to remove Todos

---
+++ 15-todo-list-project/todo-list-project/src/NewTodoForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class NewTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state);
+    this.setState({ task: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor='task'>New Todo:</label>
+        <input
+          id='task'
+          name='task'
+          type='text'
+          placeholder='New Todo'
+          value={this.state.task}
+          onChange={this.handleChange}
+        />
+        <button>Add Todo</button>
+      </form>
+    );
+  }
+}
+
+export default NewTodoForm;
