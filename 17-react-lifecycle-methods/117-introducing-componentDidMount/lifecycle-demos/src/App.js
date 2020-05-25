cf78bbd8750059e37210fa9646bd50c88a08fccb
Steven Torrence <storrence88@gmail.com>
2020-05-24 22:49:31 -0500

    Loading data with async functions

--- 17-react-lifecycle-methods/117-introducing-componentDidMount/lifecycle-demos/src/App.js
+++ 17-react-lifecycle-methods/117-introducing-componentDidMount/lifecycle-demos/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Timer from './Timer';
 import ZenQuote from './zenQuote';
+import GithubUserInfo from './GithubUserInfo';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <ZenQuote />
+        <GithubUserInfo username='facebook' />
+        <GithubUserInfo username='storrence88' />
+        <GithubUserInfo username='gaearon' />
+        <GithubUserInfo username='sophiebits' />
       </div>
     );
   }
