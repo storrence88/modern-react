759b75052831f5a4853a0d9bed6f99194aa81615
Steven Torrence <storrence88@gmail.com>
2020-05-25 14:26:25 -0500

    Fetching new cards with AJAX

---
+++ 18-lifecycle-methods-and-api-exercises/124-cards-api-project/cards-api-project/src/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a Card component</h1>
+      </div>
+    );
+  }
+}
+
+export default Card;
---
+++ 18-lifecycle-methods-and-api-exercises/124-cards-api-project/cards-api-project/src/Deck.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Card from './Card';
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck';
+
+class Deck extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { deck: null, drawn: [] };
+  }
+
+  async componentDidMount() {
+    let deck = await axios.get(`${API_BASE_URL}/new/shuffle`);
+    this.setState({ deck: deck.data });
+  }
+
+  getCard = async () => {
+    let id = this.state.deck.deck_id;
+    let cardUrl = `${API_BASE_URL}/${id}/draw/`;
+
+    try {
+      let cardRes = await axios.get(cardUrl);
+
+      if (!cardRes.data.success) {
+        throw new Error('No cards remaining!');
+      }
+
+      let card = cardRes.data.cards[0];
+      this.setState(prevState => ({
+        drawn: [
+          ...prevState.drawn,
+          {
+            id: card.code,
+            image: card.image,
+            name: `${card.value} of ${card.suit}`
+          }
+        ]
+      }));
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Card Dealer</h1>
+        <button onClick={this.getCard}>Get Card!</button>
+        <Card />
+      </div>
+    );
+  }
+}
+
+export default Deck;
