21596070243c452466a2b99e367b26279a2286c5
Steven Torrence <storrence88@gmail.com>
2020-05-26 21:15:04 -0500

    Building the dad jokes app

---
+++ 19-building-the-dad-jokes-app/dad-jokes-app/src/Joke.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Joke extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Joke component!</h1>
+      </div>
+    );
+  }
+}
