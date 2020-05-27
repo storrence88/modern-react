952066e8b7fb996866ecaebff0516af16b130ed4
Steven Torrence <storrence88@gmail.com>
2020-05-26 20:29:51 -0500

    Add game aspect to deck demo

--- 18-lifecycle-methods-and-api-exercises/124-cards-api-project/cards-api-project/src/Deck.js
+++ 18-lifecycle-methods-and-api-exercises/124-cards-api-project/cards-api-project/src/Deck.js
@@ -10,9 +10,13 @@ class Deck extends Component {
     this.state = { deck: null, drawn: [] };
   }
 
-  async componentDidMount() {
+  fetchDeck = async () => {
     let deck = await axios.get(`${API_BASE_URL}/new/shuffle`);
-    this.setState({ deck: deck.data });
+    this.setState({ deck: deck.data, drawn: [] });
+  };
+
+  async componentDidMount() {
+    this.fetchDeck();
   }
 
   getCard = async () => {
@@ -48,12 +52,44 @@ class Deck extends Component {
     ));
     return (
       <div className='Deck'>
-        <h1 className='Deck-title'>♦ Card Dealer ♦</h1>
-        <h2 className='Deck-title subtitle'>♦ A little demo made with React.js ♦</h2>
+        <h1 className='Deck-title'>🍻 King's Cup! 🍻</h1>
+        <h2 className='Deck-title subtitle'>♦ A drinking game demo made with React.js ♦</h2>
         <button className='Deck-btn' onClick={this.getCard}>
-          Get Card!
+          Draw Card!
+        </button>
+        <button className='Deck-btn' onClick={this.fetchDeck}>
+          Reset Deck
         </button>
         <div className='Deck-card-area'>{cards}</div>
+        <h2 className='Deck-title subtitle'>Rules</h2>
+        <p>
+          A: Waterfall - Every player drinks and no one stops until the player before them
+          (clockwise) does
+        </p>
+        <p>2: You - Pick someone to take a drink</p>
+        <p>3: Me - Player who drew the card takes a drink</p>
+        <p>4: Whores: All ladies take a drink</p>
+        <p>5: Rule Master - Create a rule. Anyone caught breaking the rule has to take a drink</p>
+        <p>6: Dicks - All guys take a drink</p>
+        <p>7: Heaven - Last person to raise their hands in the air has to take a drink</p>
+        <p>8: Mate - Pick someone to take a drink with you</p>
+        <p>
+          9: Rhyme - Choose a word. Next person has to say a word that rhymes. First one to mess up
+          takes a drink
+        </p>
+        <p>
+          10: Categories - Choose a category. Next person has to name an item from that category.
+          First one to mess up takes a drink
+        </p>
+        <p>J: Never Have I Ever - Loser of this one has to take drink</p>
+        <p>
+          Q: Question Master - Ask someone a question. If they answer with a statement, they have to
+          drink.
+        </p>
+        <p>
+          K: King's Cup - Have a beer or a shot in the center of the table. Player who draws the
+          last king has to drink thus ending the game
+        </p>
       </div>
     );
   }
