917ed2e5525d78a786b893f732764c2f364c39dc
Steven Torrence <storrence88@gmail.com>
2020-05-28 22:20:19 -0500

    Adding a loading spinner

--- 19-building-the-dad-jokes-app/dad-jokes-app/src/JokeList.js
+++ 19-building-the-dad-jokes-app/dad-jokes-app/src/JokeList.js
@@ -11,7 +11,10 @@ export default class JokeList extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]') };
+    this.state = {
+      jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'),
+      loading: false
+    };
   }
 
   componentDidMount() {
@@ -28,6 +31,7 @@ export default class JokeList extends Component {
     }
     this.setState(
       prevState => ({
+        loading: false,
         jokes: [...prevState.jokes, ...jokes]
       }),
       () => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
@@ -47,10 +51,18 @@ export default class JokeList extends Component {
   };
 
   handleClick = () => {
-    this.getJokes();
+    this.setState({ loading: true }, this.getJokes);
   };
 
   render() {
+    if (this.state.loading) {
+      return (
+        <div className='JokeList-spinner'>
+          <i className='far fa-10x fa-laugh fa-spin' />
+          <h1 className='JokeList-title'>Loading...</h1>
+        </div>
+      );
+    }
     return (
       <div className='JokeList'>
         <div className='JokeList-sidebar'>
