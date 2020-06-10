8b0fac1346f494a85259f2ed1e551550db1fb8b5
Steven Torrence <storrence88@gmail.com>
2020-06-09 22:30:03 -0500

    Couple of small tweaks

--- 23-react-router-dog-finder/dog-finder/src/DogList.js
+++ 23-react-router-dog-finder/dog-finder/src/DogList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './DogList.css';
 
 export default class DogList extends Component {
@@ -6,15 +7,15 @@ export default class DogList extends Component {
     return (
       <div className='DogList'>
         <h1 className='display-1 text-center'>Dog List!</h1>
-        <div className='container'>
-          <div className='row'>
-            {this.props.dogs.map(dog => (
-              <div className='Dog col-lg-4 text-center' key={dog.name}>
-                <img src={dog.src} alt={dog.name} />
+        <div className='row'>
+          {this.props.dogs.map(dog => (
+            <div className='Dog col-lg-4 text-center' key={dog.name}>
+              <img src={dog.src} alt={dog.name} />
+              <Link to={`/dogs/${dog.name.toLowerCase()}`}>
                 <h3>{dog.name}</h3>
-              </div>
-            ))}
-          </div>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     );
