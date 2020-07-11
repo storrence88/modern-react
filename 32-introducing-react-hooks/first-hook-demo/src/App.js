172fba0d3ebb0f9e435c9829795dabd26b84e691
Steven Torrence <storrence88@gmail.com>
2020-07-10 22:47:33 -0500

    First react hook demo

---
+++ 32-introducing-react-hooks/first-hook-demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CounterHook from './CounterHook';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <CounterHook />
+    </div>
+  );
+}
+
+export default App;
