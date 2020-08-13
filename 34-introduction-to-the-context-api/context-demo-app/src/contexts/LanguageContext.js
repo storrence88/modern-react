ba0753da7204c3e6664958f867314a578a4f880e
Steven Torrence <storrence88@gmail.com>
2020-08-12 20:03:13 -0500

    Consuming multiple contexts in the same class component

--- 34-introduction-to-the-context-api/context-demo-app/src/contexts/LanguageContext.js
+++ 34-introduction-to-the-context-api/context-demo-app/src/contexts/LanguageContext.js
@@ -20,3 +20,9 @@ export class LanguageProvider extends Component {
     );
   }
 }
+
+export const withLanguageContext = (Component) => (props) => (
+  <LanguageContext.Consumer>
+    {(value) => <Component languageContext={value} {...props} />}
+  </LanguageContext.Consumer>
+);
