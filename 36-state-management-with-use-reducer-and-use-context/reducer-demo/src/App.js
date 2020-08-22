b2cd63fae857a8b626ba57895d1401eec0accc99
Steven Torrence <storrence88@gmail.com>
2020-08-22 15:34:28 -0500

    Create reducer demo

---
+++ 36-state-management-with-use-reducer-and-use-context/reducer-demo/src/App.js
@@ -0,0 +1,32 @@
+import React, { useReducer } from 'react';
+import './App.css';
+
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + action.amount };
+    case 'DECREMENT':
+      return { count: state.count - action.amount };
+    case 'RESET':
+      return { count: 0 };
+    default:
+      return `No action called ${action.type}`;
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(countReducer, { count: 0 });
+
+  return (
+    <div className='App'>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: 'INCREMENT', amount: 1 })}>Add 1</button>
+      <button onClick={() => dispatch({ type: 'INCREMENT', amount: 5 })}>Add 5</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT', amount: 1 })}>Subtract 1</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT', amount: 5 })}>Subtract 5</button>
+      <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
+    </div>
+  );
+};
+
+export default App;
