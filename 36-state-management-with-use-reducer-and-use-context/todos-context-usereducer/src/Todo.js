adb1e306413033bff37ebd5a686d635fe5b7df4a
Steven Torrence <storrence88@gmail.com>
2020-08-26 21:09:28 -0500

    Add React.memo to reduce rerenders and optimize code

--- 36-state-management-with-use-reducer-and-use-context/todos-context-usereducer/src/Todo.js
+++ 36-state-management-with-use-reducer-and-use-context/todos-context-usereducer/src/Todo.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, memo } from 'react';
 import useToggleState from './hooks/useToggleState';
 import EditTodoForm from './EditTodoForm';
 import ListItem from '@material-ui/core/ListItem';
@@ -42,4 +42,4 @@ const Todo = ({ id, task, completed }) => {
   );
 };
 
-export default Todo;
+export default memo(Todo);
